530d2dba0435d82cab3badf4e23c9f18f781cb39
Dheeraj587 <thapadheeraj857@gmail.com>
2026-06-22 17:15:38 +0530

    this is my final update

--- src/constant/__init__.py
+++ src/constant/__init__.py
@@ -1,11 +1,11 @@
 import os
 
-AWS_S3_BUCKET_NAME = " "
+AWS_S3_BUCKET_NAME = "sensor-fault-001"
 MONGO_DATABASE_NAME = "Dheeraj"
 MONGO_COLLECTION_NAME = "waferfault"
 
 TARGET_COLUMN = "quality"
-MONGO_DB_URL = MONGO_DB_URL = MONGO_DB_URL = "mongodb+srv://thapadheeraj857_db_user:Dheeraj123@cluster001.15qn1hx.mongodb.net/Dheeraj?retryWrites=true&w=majority&tls=true"
+MONGO_DB_URL = "mongodb+srv://thapadheeraj857_db_user:Dheeraj123@cluster001.15qn1hx.mongodb.net/Dheeraj?retryWrites=true&w=majority&tls=true"
 
 MONGO_FILE_NAME = "quality"
 MODEL_FILE_EXTENSION = ".pkl "
